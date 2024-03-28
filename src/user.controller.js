a8c6ef75426c065d5cdc338dd464876dacee64dc
swapnaBommu <swapnabommu8143@gmail.com>
2024-03-28 15:15:20 +0530

    register user and encrypting password

--- src/user.controller.js
+++ src/user.controller.js
@@ -1,66 +1,52 @@
-import UserModel from './user.model.js';
-import UserRepository from './user.repository.js';
+import User from './user.schema.js';
+// import UserRepository from './user.repository.js';
 import bcrypt from 'bcrypt';
 
 export default class UserController {
 
-  constructor(){
-    this.userRepository = new UserRepository();
-  }
   async signUp(req, res) {
-    const {
-      name,
-      email,
-      password,
-      type,
-    } = req.body;
+    const { name, email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const user = new UserModel(
-      name,
-      email,
-      hashedPassword,
-      type
-    );
-    await this.userRepository.singup(user);
-    res.status(201).send(user);
-    res.render("signin");
+    const user = await User.create({name,email,password});
+    res.status(201).json({
+      success:true,
+    })
   }
 
-  async signIn(req, res, next) {
-    try{
-      // 1. Find user by email.
-    const user = await this.userRepository.findByEmail(req.body.email);
-    if(!user){
-      return res
-        .status(400)
-        .send('Incorrect Credentials');
-    }else{
-      // 2. Compare password with hashed password.
-      const result = await bcrypt.compare(req.body.password, user.password);
-      if(result){
- // 3. Create token.
- const token = jwt.sign(
-  {
-    userID: user._id,
-    email: user.email,
-  },
-  'AIb6d35fvJM4O9pXqXQNla2jBCH9kuLz',
-  {
-    expiresIn: '1h',
-  }
-);
-// 4. Send token.
-return res.status(200).send(token);
-      }else{
-        return res
-        .status(400)
-        .send('Incorrect Credentials');
-      }
-    }
-    }catch(err){
-      console.log(err);
-      return res.status(200).send("Something went wrong");
-    }
-  }
+//   async signIn(req, res, next) {
+//     try{
+//       // 1. Find user by email.
+//     const user = await this.userRepository.findByEmail(req.body.email);
+//     if(!user){
+//       return res
+//         .status(400)
+//         .send('Incorrect Credentials');
+//     }else{
+//       // 2. Compare password with hashed password.
+//       const result = await bcrypt.compare(req.body.password, user.password);
+//       if(result){
+//  // 3. Create token.
+//  const token = jwt.sign(
+//   {
+//     userID: user._id,
+//     email: user.email,
+//   },
+//   'AIb6d35fvJM4O9pXqXQNla2jBCH9kuLz',
+//   {
+//     expiresIn: '1h',
+//   }
+// );
+// // 4. Send token.
+// return res.status(200).send(token);
+//       }else{
+//         return res
+//         .status(400)
+//         .send('Incorrect Credentials');
+//       }
+//     }
+//     }catch(err){
+//       console.log(err);
+//       return res.status(200).send("Something went wrong");
+//     }
+//   }
 }
--- src/user.schema.js
+++ src/user.schema.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcrypt';
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -22,5 +22,14 @@ const userSchema = new mongoose.Schema({
 },
 {timestamps:true}
 );
+//Encrypting password before saving the user
+userSchema.pre('save',async function(next){
+    if(!this.isModified("password")){
+        next();
+    }
+    this.password = await bcrypt.hash(this.password,10);
+})
+
+
 
 export default mongoose.model("User",userSchema);
